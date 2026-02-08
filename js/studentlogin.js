cd9a71d79fe14c64a4f4eb2797c8f24169d55880
jrsantiagoo <jrsantiago3001@gmail.com>
2026-02-08 09:47:43 +0800

    studentlogin.js done

--- js/studentlogin.js
+++ js/studentlogin.js
@@ -1,3 +1,24 @@
-var username = document.getElementById('studentusername')
-var password = document.getElementById('studentpassword')
+function checkForm(){
+    var username = document.getElementById('studentusername');
+    var password = document.getElementById('studentpassword');
+    var errormess = document.getElementById('errormess');
 
+    var all_fields = true;
+    username.style.borderColor = "var(--midgrey-green)";
+    password.style.borderColor = "var(--midgrey-green)";
+    if (username.value == ""){
+        all_fields = false;
+        username.style.borderColor = "red";
+    }
+    if (password.value  == ""){
+        all_fields = false;
+        password.style.borderColor = "red";
+    }
+
+    if (all_fields == false){
+        errormess.style.opacity = 100;
+        errormess.innerHTML = "Please fill out all fields."
+    }
+
+    return all_fields
+}
