7ed7255ad96839979d855461dc4687123f5f9914
chazinocencio <chazinocencio@gmail.com>
2026-02-08 21:43:49 +0800

    profile logouts working

--- js/student.js
+++ js/student.js
@@ -1,5 +1,6 @@
 const studentprofile = document.getElementById('studentprofile')
 const studentreserve = document.getElementById('studentreserve')
+const logout = document.getElementById('logout')
 
 studentprofile.addEventListener('click', function(){
 
@@ -8,4 +9,8 @@ studentprofile.addEventListener('click', function(){
 
 studentreserve.addEventListener('click', function(){
         window.location.href = "../studentreserve.html"
+})
+
+logout.addEventListener('click', function(){
+        window.location.href = "../index.html"
 })
--- js/technician.js
+++ js/technician.js
@@ -1,5 +1,9 @@
 const technicianprofile = document.getElementById('technicianprofile')
+const logout = document.getElementById('logout')
 
 technicianprofile.addEventListener('click', function(){
         window.location.href = "technicianprofile.html"
+})
+logout.addEventListener('click', function(){
+        window.location.href = "../index.html"
 })
