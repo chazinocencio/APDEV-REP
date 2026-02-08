2dad8ef5fc7667423d18455d4788e8a607864a04
chazinocencio <chazinocencio@gmail.com>
2026-02-08 18:01:03 +0800

    done with student view and other rooms

--- js/cloudfair.js
+++ js/cloudfair.js
@@ -3,7 +3,7 @@ const checkbox = document.getElementById('verify');
 checkbox.addEventListener('change', function(){
 
     var ver = document.getElementById("ver");
-    if (ver) ver.textContent = "Verifying"
+    if (ver) ver.textContent = "Verifying..."
 
     if(this.checked){
         window.location.href = "../index.html"
--- js/studentreserve.js
+++ js/studentreserve.js
@@ -1,5 +1,10 @@
 const studentprofile = document.getElementById('back')
 const g301 = document.getElementById('G301')
+const g302 = document.getElementById('G302')
+const g303 = document.getElementById('G303')
+const g304 = document.getElementById('G304')
+const g305 = document.getElementById('G305')
+const g306 = document.getElementById('G306')
 
 studentprofile.addEventListener('click', function(){
     window.location.href = "../student.html";
@@ -7,4 +12,24 @@ studentprofile.addEventListener('click', function(){
 
 g301.addEventListener('click', function(){
     window.location.href = "../studentrooms/G301.html";
+})
+
+if (g302) g302.addEventListener('click', function(){
+    window.location.href = "../studentrooms/G302.html";
+})
+
+if (g303) g303.addEventListener('click', function(){
+    window.location.href = "../studentrooms/G303.html";
+})
+
+if (g304) g304.addEventListener('click', function(){
+    window.location.href = "../studentrooms/G304.html";
+})
+
+if (g305) g305.addEventListener('click', function(){
+    window.location.href = "../studentrooms/G305.html";
+})
+
+if (g306) g306.addEventListener('click', function(){
+    window.location.href = "../studentrooms/G306.html";
 })
