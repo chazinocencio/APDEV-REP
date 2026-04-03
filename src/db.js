0a2e42524e1cca9d8a78f013069c71e11ed741ed
jrsantiagoo <jrsantiago3001@gmail.com>
2026-04-03 23:57:42 +0800

    chore: add .env to gitignore

--- src/db.js
+++ src/db.js
@@ -9,7 +9,9 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ccapdev-la
 export const connectDB = async () => {
     try {
         //change "sample" to database name
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(MONGO_URI, {
+            dbName: 'ccapdev-lab-reservation'
+        });
         console.log("MongoDB connected!");
     } catch (error) {
         console.log("Unable to connect to MongoDB.");
